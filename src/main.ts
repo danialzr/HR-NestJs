a556f436936b8445f3da74953b7ffdd0d35c34a9
danialzr <mobinzargham@gmail.com>
2025-12-28 00:35:00 +0330

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT', 3001)
+  const appName = configService.get('APP_NAME')
+
+  await app.listen(port);
+  console.log(`🔫 Application "${appName}" is running on: ${port}`)
+}
+bootstrap();
