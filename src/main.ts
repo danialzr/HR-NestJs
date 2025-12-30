97d2ce371f2825d53286871e166bc4b80ef68651
danialzr <mobinzargham@gmail.com>
2025-12-30 23:32:27 +0330

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,12 +10,60 @@ async function bootstrap() {
   const port = configService.get('PORT', 3001)
   const appName = configService.get('APP_NAME')
 
+  //set global API perfix
+  app.setGlobalPrefix('api/v1')
+
   // For DTO
   app.useGlobalPipes(new ValidationPipe({
   whitelist: true, 
   forbidNonWhitelisted: true, 
   transform: true, 
-}));
+  }));
+
+  //Add Swagger
+  //manager swagger
+  const managerConfig = new DocumentBuilder()
+    .setTitle('Hr API - manager routes')
+    .setDescription('this route panel for manager')
+    .setVersion('1.0')
+    .build();
+  
+  const managerDocument = SwaggerModule.createDocument(app, managerConfig, {
+    include: [AppModule],
+    deepScanRoutes: true
+  });
+
+  if (managerDocument.paths) {
+    Object.keys(managerDocument.paths).forEach((path) => {
+      if (!path.includes('/manager') && !path.includes('/auth')) {
+        delete managerDocument.paths[path];
+      }
+    })
+  }
+
+  SwaggerModule.setup('api/v1/manager/docs', app, managerDocument)
+
+  //employee swagger
+  const employeeConfig = new DocumentBuilder()
+    .setTitle('Hr API - employee routes')
+    .setDescription('this route panel for employee')
+    .setVersion('1.0')
+    .build();
+  
+  const employeeDocument = SwaggerModule.createDocument(app, employeeConfig, {
+    include: [AppModule],
+    deepScanRoutes: true
+  });
+
+  if (employeeDocument.paths) {
+    Object.keys(employeeDocument.paths).forEach((path) => {
+      if (!path.includes('/employee') && !path.includes('/auth')) {
+        delete employeeDocument.paths[path];
+      }
+    })
+  }
+
+  SwaggerModule.setup('api/v1/employee/docs', app, employeeDocument)
 
   await app.listen(port);
   console.log(`🔫 Application "${appName}" is running on: ${port}`)
--- src/modules/auth/dto/register.dto.ts
+++ src/modules/auth/dto/register.dto.ts
@@ -1,11 +1,14 @@
 import { IsEnum, IsOptional, IsString } from "class-validator";
 import { Role } from "../../../shared/enums/user-role.enum";
+import { ApiProperty } from "@nestjs/swagger";
 
 
 export class RegisterDto {
+    @ApiProperty({ description: 'mobile number', example: '09370947629'})
     @IsString()
     mobile: string;
 
+    @ApiProperty({ description: 'password', example: '12345678'})
     @IsString()
     password: string;
 
